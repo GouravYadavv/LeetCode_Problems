d88672d1d7badd89d76120ed2be38ffa054f16ed
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-10-04 01:05:51 +0530

    Time: 856 ms (5.01%), Space: 18.4 MB (27.96%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a=set(nums)
+        for i in a:
+            if nums.count(i)==1:
+                return i
+            else:
+                continue
