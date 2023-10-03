7fd7409487c769052e8343fc0f26ccf22d42e8d0
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-10-03 09:09:18 +0530

    Time: 45 ms (27.06%), Space: 16.2 MB (82.24%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -3,11 +3,8 @@ class Solution:
         result={}
         res=0
 
-        for num in nums:
-            if num in result:
-                res+=result[num]
-                result[num]+=1
-            else:
-                result[num]=1
-        
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    res+=1
         return res
