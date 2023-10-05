aff589d5268008ef8bc813f8cd6a7a48b54d0223
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-10-05 14:29:16 +0530

    Time: 106 ms (81.01%), Space: 17.6 MB (84.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,3 +1,15 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        return [i for i in set(nums) if nums.count(i)>len(nums)//3]
+    def majorityElement(self, nums: list[int]) -> list[int]:
+        # Create a Counter to store the count of each element
+        element_count = Counter(nums)
+        
+        majority_elements = []
+        threshold = len(nums) // 3
+        
+        # Iterate through the element count to identify majority elements
+        for element, count in element_count.items():
+            # Check if the element count is greater than the threshold
+            if count > threshold:
+                majority_elements.append(element)
+        
+        return majority_elements
