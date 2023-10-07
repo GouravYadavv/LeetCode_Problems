689638f27472986c9aed2ae64feadb2cb15425c5
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-10-07 09:58:47 +0530

    Time: 145 ms (82.39%), Space: 17.9 MB (51.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,6 @@
-from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         majority=len(nums)//2 # This is the majority as per the question.
-        a=Counter(nums)
-        for i,j in a.items():
-            if j > majority:
-                return  i
+        for i in set(nums):
+            if nums.count(i)>majority:
+                return i
