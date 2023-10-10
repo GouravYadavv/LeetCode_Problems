689b670fa0b67b294c95466bdb1656851ef1d130
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-10-10 18:38:36 +0530

    Time: 1457 ms (5.26%), Space: 35.2 MB (71.05%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.py
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.py
@@ -2,11 +2,21 @@ class Solution:
     def minOperations(self, nums: List[int]) -> int:
         n = len(nums)
         nums = sorted(set(nums))
-        ans = sys.maxsize
-
-        for i, s in enumerate(nums):
-            e = s + n - 1
-            idx = bisect_right(nums, e)
-
-            ans = min(ans, n - (idx - i))
-        return ans
+		
+        answer = float("+inf")
+        for i, start in enumerate(nums):
+            
+            search = start + n - 1  # number to search
+            start, end = 0, len(nums)-1
+            
+            while start <= end:
+                mid = start + (end - start) // 2
+                if nums[mid] <= search:
+                    idx = mid
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            
+            changes = idx - i + 1
+            answer = min(answer, n - changes)
+        return answer
