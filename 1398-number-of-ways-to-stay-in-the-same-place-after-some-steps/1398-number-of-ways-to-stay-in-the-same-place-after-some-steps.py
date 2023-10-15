73582dc4ac2e4b7eff36802bf4f7245f774a948c
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-10-15 21:55:41 +0530

    Time: 711 ms (9.44%), Space: 93.8 MB (33.33%) - LeetHub

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,14 +1,23 @@
 class Solution:
+    MOD = 1000000007
+
     def numWays(self, steps: int, arrLen: int) -> int:
-        modulo = 1000000007
-        max_len = min(arrLen, 1 + steps // 2)
-        ways = [0] * (max_len + 1)
-        ways[0] = 1
+        return self.countWays(steps, 0, arrLen)
+    dp={}
+    def countWays(self, steps, position, arrLen):
+        if position < 0 or position >= arrLen:
+            return 0
 
-        for i in range(steps):
-            left = 0
+        if steps == 0:
+            return 1 if position == 0 else 0
+        elif (steps, position,arrLen) in self.dp:
+            return self.dp[(steps, position,arrLen)]
 
-            for j in range(min(max_len, i + 2, steps - i + 3)):
-                left, ways[j] = ways[j], (ways[j] + left + ways[j + 1]) % modulo
+        ways = self.countWays(steps - 1, position, arrLen)
+        ways = (ways + self.countWays(steps - 1, position - 1, arrLen)) % self.MOD
+        ways = (ways + self.countWays(steps - 1, position + 1, arrLen)) % self.MOD
+        self.dp[(steps, position,arrLen)] = ways
+        
 
-        return ways[0]
+        return ways
+
