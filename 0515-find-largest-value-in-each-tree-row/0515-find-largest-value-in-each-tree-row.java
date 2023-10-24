6da5d9c02e76b68962d2a8bf3d1e60aa7188046a
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-10-24 08:27:13 +0530

    Time: 2 ms (82.84%), Space: 44 MB (61.01%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -1,48 +1,35 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 public class Solution {
     public List<Integer> largestValues(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        
         if (root == null) {
-            return res;
+            return Collections.emptyList();
         }
-        
-        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
-        stack.push(new Pair<>(root, 0));
-        
-        while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> pair = stack.pop();
-            TreeNode node = pair.getKey();
-            int level = pair.getValue();
-            
-            if (level == res.size()) {
-                res.add(node.val);
-            } else {
-                res.set(level, Math.max(res.get(level), node.val));
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<Integer> res = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int maxVal = Integer.MIN_VALUE;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                maxVal = Math.max(maxVal, node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
-            
-            if (node.left != null) {
-                stack.push(new Pair<>(node.left, level + 1));
-            }
-            if (node.right != null) {
-                stack.push(new Pair<>(node.right, level + 1));
-            }
+
+            res.add(maxVal);
         }
-        
+
         return res;
     }
 }
