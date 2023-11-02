c9765aa5d6d6e390984c063ea85ad4cef27ddcbe
Gourav Yadav <107845874+GouravYadavv@users.noreply.github.com>
2023-11-02 14:37:54 +0530

    Time: 57 ms (19.12%), Space: 17.3 MB (96.93%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
@@ -1,25 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
-        result=0
+class Solution(object):
+    def __init__(self):
+        self.result = 0
 
-        def dfs(node):
-            nonlocal result
-            if not node:
-                return 0,0
-            leftSum,leftCount=dfs(node.left)
-            rightSum,rightCount=dfs(node.right)
-            curr_sum=node.val+leftSum+rightSum
-            curr_count=1+leftCount+rightCount
-            if curr_sum//curr_count==node.val:
-                result+=1
-            return curr_sum,curr_count
-        
-        dfs(root)
-        return result
-    
+    def averageOfSubtree(self, root):
+        self.dfs(root)
+        return self.result
+
+    def dfs(self, node):
+        if not node:
+            return [0, 0]
+
+        left = self.dfs(node.left)
+        right = self.dfs(node.right)
+
+        currentSum = left[0] + right[0] + node.val
+        currentCount = left[1] + right[1] + 1
+
+        if currentSum // currentCount == node.val:
+            self.result += 1
+
+        return [currentSum, currentCount]
